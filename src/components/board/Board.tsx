7c0175ae2f2780a8342c46f167cfc3621bd5abdd
Hamza <hamza.khalifa.clashingames@gmail.com>
2023-10-27 08:37:10 +0200

    Console logs removed

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -130,7 +130,6 @@ const Board: React.FunctionComponent<IBoard> = (props: IBoard) => {
       const character = aiActions.aiCharactersToAct[0];
 
       let chainOfPositionsToClick: IPosition[] = [];
-      console.log("teamBForAi.current", teamBForAi.current);
       const actions = getAiCharacterTurnActions(character, {
         totalColumns: props.columns,
         totalRows: props.rows,
